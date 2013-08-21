16729b43229a2fdc0b299718d1d0fb1555450369
Charles Chamberlain <charlespipin@gmail.com>
2013-08-21 10:48:41 -0500

    Better unit display, unit edit, moved CSS

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,5 @@
 class Unit < ActiveRecord::Base
-  attr_accessible :delay, :location, :kind
+  attr_accessible :delay, :location, :kind, :destination
 	validates :user_id, :presence => true
 	belongs_to :user
 	
