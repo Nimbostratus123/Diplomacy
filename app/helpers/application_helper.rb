41e9c97987d56a765d8784a7769385204f73571a
Charles Chamberlain <charlespipin@gmail.com>
2013-09-02 19:02:18 -0500

    Slight change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -180,7 +180,7 @@ module ApplicationHelper
 			'skagerrak' => [520, 350], 
 			'tyrrhenian sea' => [475, 810],
 			'switzerland' => [440, 640],
-			'snake island' => [860, 680],
+			'snake island' => [950, 650],
 			'middle of nowhere' => [-50, -50]
 		}
 	end
